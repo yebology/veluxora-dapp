1c13890ec9eb4fafc5d51fef07114da0fdfc88b5
yebology <139052533+yebology@users.noreply.github.com>
2025-05-15 13:22:05 +0700

    done connect reown

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import {
+  createAppKit,
+  useAppKit,
+  useAppKitAccount,
+  useAppKitProvider,
+} from "@reown/appkit/react";
+import { optimismSepolia, type AppKitNetwork } from "@reown/appkit/networks";
+import { EthersAdapter } from "@reown/appkit-adapter-ethers";
+import { truncate } from "./utils/helper";
+
+const projectId = import.meta.env.PROJECT_ID;
+
+const networks: [AppKitNetwork] = [optimismSepolia];
+
+const metadata = {
+  name: "Veluxora",
+  description: "",
+  url: "",
+  icons: [""],
+};
+
+createAppKit({
+  adapters: [new EthersAdapter()],
+  networks,
+  metadata,
+  projectId,
+});
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { open } = useAppKit();
+  const { address, isConnected } = useAppKitAccount();
+  const { walletProvider } = useAppKitProvider("eip155");
+
+  useEffect(() => {
+    console.log("Address");
+  }, [isConnected]);
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button
+          className="rounded-xl p-2 shadow-sm font-bold text-white bg-pink-500"
+          onClick={() => open()}
+        >
+          {address ? truncate(address, 4, 4, 11) : "Connect Wallet"}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
