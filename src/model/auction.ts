cc6be8c9cb87b7f76001eeb0dc4edb5dd15d3f9a
yebology <139052533+yebology@users.noreply.github.com>
2025-06-08 18:43:16 +0700

    update auction interface

--- src/model/auction.ts
+++ src/model/auction.ts
@@ -1,16 +1,16 @@
 export interface Auction {
     id: string,
-    name: string,
-    image: string,
-    description: string,
-    creator: string,
-    bpkbId: number,
-    stnkId: number,
-    minBid: number,
-    highestBid: number,
-    highestBidder: string,
-    startTime: number,
-    endTime: number,
-    claimed: boolean,
-    canceled: boolean,
+    name: string, // db
+    image: string, // db
+    description: string, // db
+    creator: string, // smart contract
+    tokenId: number, // smart contract
+    minBid: number, // smart contract
+    highestBid: number, // smart contract
+    highestBidder: string, // smart contract
+    startTime: number, // smart contract
+    endTime: number, // smart contract
+    claimed: boolean, // smart contract
+    canceled: boolean, // smart contract
+    // kalo mau nambah atribut tambahan apapun simpen di db
 }
