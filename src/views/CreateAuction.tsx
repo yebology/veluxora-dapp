de2ba629762f450f61f63933f398b59e97b22666
yebology <139052533+yebology@users.noreply.github.com>
2025-06-08 19:11:25 +0700

    update nft metadata

--- src/views/CreateAuction.tsx
+++ src/views/CreateAuction.tsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import { pinata } from "../global/pinata";
 
 export const CreateAuction = (docs: File) => {
-    
   const uploadFile = async () => {
     try {
       return (
@@ -21,26 +20,64 @@ export const CreateAuction = (docs: File) => {
         name: name,
         image: image,
         description: description,
-        vehicle: {
-          plateNumber: plateNumber,
-          brand: brand,
-          model: model,
-          year: parseInt(year),
-          color: color,
-        },
-        stnk: {
-          number: stnkNumber,
-          issuedBy: stnkIssuedBy,
-          issuedDate: stnkIssuedDate,
-          validUntil: stnkValidUntil,
-          encryptedFileUrl: stnkEncryptedFileUrl,
-        },
-        bpkb: {
-          number: bpkbNumber,
-          issuedBy: bpkbIssuedBy,
-          issuedDate: bpkbIssuedDate,
-          encryptedFileUrl: bpkbEncryptedFileUrl,
-        },
+        attributes: [
+          {
+            trait_type: "Plate Number",
+            value: plateNumber,
+          },
+          {
+            trait_type: "Brand",
+            value: brand,
+          },
+          {
+            trait_type: "Model",
+            value: model,
+          },
+          {
+            trait_type: "Year",
+            value: parseInt(year),
+          },
+          {
+            trait_type: "Color",
+            value: color,
+          },
+          {
+            trait_type: "STNK Number",
+            value: stnkNumber,
+          },
+          {
+            trait_type: "STNK Issued By",
+            value: stnkIssuedBy,
+          },
+          {
+            trait_type: "STNK Issued Date",
+            value: stnkIssuedDate,
+          },
+          {
+            trait_type: "STNK Valid Until",
+            value: stnkValidUntil,
+          },
+          {
+            trait_type: "STNK File",
+            value: stnkEncryptedFileUrl,
+          },
+          {
+            trait_type: "BPKB Number",
+            value: bpkbNumber,
+          },
+          {
+            trait_type: "BPKB Issued By",
+            value: bpkbIssuedBy,
+          },
+          {
+            trait_type: "BPKB Issued Date",
+            value: bpkbIssuedDate,
+          },
+          {
+            trait_type: "BPKB File",
+            value: bpkbEncryptedFileUrl,
+          },
+        ],
       });
       return "https://gateway.pinata.cloud/" + file.cid;
     } catch (error) {
@@ -49,26 +86,26 @@ export const CreateAuction = (docs: File) => {
     }
   };
 
-  const [name, setName] = useState("")
-  const [image, setImage] = useState("")
-  const [description, setDescription] = useState("")
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("");
+  const [description, setDescription] = useState("");
 
-  const [plateNumber, setPlateNumber] = useState("")
-  const [brand, setBrand] = useState("")
-  const [model, setModel] = useState("")
-  const [year, setYear] = useState("")
-  const [color, setColor] = useState("")
+  const [plateNumber, setPlateNumber] = useState("");
+  const [brand, setBrand] = useState("");
+  const [model, setModel] = useState("");
+  const [year, setYear] = useState("");
+  const [color, setColor] = useState("");
 
-  const [stnkNumber, setStnkNumber] = useState("")
-  const [stnkIssuedBy, setStnkIssuedBy] = useState("")
-  const [stnkIssuedDate, setStnkIssuedDate] = useState("")
-  const [stnkValidUntil, setStnkValidUntil] = useState("")
-  const [stnkEncryptedFileUrl, setStnkEncryptedFileUrl] = useState("")
-  
-  const [bpkbNumber, setBpkbNumber] = useState("")
-  const [bpkbIssuedBy, setBpkbIssuedBy] = useState("")
-  const [bpkbIssuedDate, setBpkbIssuedDate] = useState("")
-  const [bpkbEncryptedFileUrl, setBpkbEncryptedFileUrl] = useState("")
+  const [stnkNumber, setStnkNumber] = useState("");
+  const [stnkIssuedBy, setStnkIssuedBy] = useState("");
+  const [stnkIssuedDate, setStnkIssuedDate] = useState("");
+  const [stnkValidUntil, setStnkValidUntil] = useState("");
+  const [stnkEncryptedFileUrl, setStnkEncryptedFileUrl] = useState("");
+
+  const [bpkbNumber, setBpkbNumber] = useState("");
+  const [bpkbIssuedBy, setBpkbIssuedBy] = useState("");
+  const [bpkbIssuedDate, setBpkbIssuedDate] = useState("");
+  const [bpkbEncryptedFileUrl, setBpkbEncryptedFileUrl] = useState("");
 
   return (
     <div>
